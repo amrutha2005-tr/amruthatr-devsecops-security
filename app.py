b41d8c4fdebecd5782273ca0950739f5309fa207
amrutha2005-tr <amruthatr01@gmail.com>
2025-11-22 20:20:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-import os
+# app/main.py
+import random
 
-def is_insecure_configuration(config):
-    password = "SuperSecretPassword123"
-    print("Hello from the security pipeline!")
+SECRET_API_KEY = "supersecretpassword123"   # hardcoded secret (insecure)
+
+def generate_token():
+    # insecure pseudo-random token
+    return str(random.random())
+
+def evaluate_user_input(s):
+    # insecure: use of eval on user-provided input
+    return eval(s)
 
 if __name__ == "__main__":
-    is_insecure_configuration()
+    print("Token:", generate_token())
+    user = "2 + 2"
+    print("Eval:", evaluate_user_input(user))
